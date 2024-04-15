5641220c14482f8772e903a760056cfb5244971d
gugenoot <162718711+gugenoot@users.noreply.github.com>
2024-04-16 00:55:12 +0700

    Add files via upload

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,7 +1,32 @@
-function toggleUserMenu() {
-    const userMenu = document.getElementById('userMenu');
-    userMenu.style.display = (userMenu.style.display === 'none' || userMenu.style.display === '') ? 'block' : 'none';
-}
+// function toggleUserMenu() {
+//     const userMenu = document.getElementById('dropdown-menu');
+//     userMenu.style.display = (userMenu.style.display === 'none' || userMenu.style.display === '') ? 'block' : 'none';
+// }
+
+const menuButton = document.querySelector(".users");
+const menuContainer = document.querySelector(".dropdown-menu");
+
+menuButton.addEventListener("click", () => {
+  if (menuContainer.classList.contains("hidden")) {
+    menuButton.classList.add("active");
+    menuContainer.classList.remove("hidden");
+    menuContainer.classList.add("show");
+  } else {
+    menuButton.classList.remove("active");
+    menuContainer.classList.remove("show");
+    menuContainer.classList.add("hidden");
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (!e.target.closest(".users") && !e.target.closest(".dropdown-menu")) {
+    menuButton.classList.remove("active");
+    menuContainer.classList.remove("show");
+    menuContainer.classList.add("hidden");
+  }
+});
+
+
 
 function toggleScrollToTopButton() {
     var scrollToTopBtn = document.getElementById('scrollToTopBtn');
